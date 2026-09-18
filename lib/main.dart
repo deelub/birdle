574f70cf30020096966ca99b18bac85a35ba6633
divine <divinelubiku6@gmail.com>
2026-09-18 13:37:07 +0200

    feat : added hit colors

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,18 @@ class Tile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width : 60,
+      height: 60,
+      decoration : BoxDecoration(
+        border : Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green, //Hit enum type
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+      ),
+    );
   }
 }
